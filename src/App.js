7f774c1cef38ec40e3f3c75b930a322602b8fff5
ragibayon <ragib.ayon@live.com>
2023-04-27 17:00:53 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+function App() {
+  const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItem = items.filter((item) => item.category === category);
+    setMenuItems(newItem);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2 className="title">Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
